f4d138ed9701641e7aef3c67eb162159e0cb3281
Turdiev <faridun.turdiev@softwareone.com>
2024-03-13 07:34:11 +0100

    Created Angular App

--- Caravan/src/app/app.component.ts
+++ Caravan/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -11,3 +12,4 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Caravan';
 }
+
