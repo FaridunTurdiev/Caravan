8d0212b062f3e9becb179f368a7fbc642f5613db
Turdiev <faridun.turdiev@softwareone.com>
2024-04-15 14:13:32 +0200

    Added menu button and fixed header

--- CaravanFrontend/src/app/components/home/home.page.ts
+++ CaravanFrontend/src/app/components/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonIcon, IonButton } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
@@ -7,7 +7,7 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonInput, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, HeaderComponent],
+  imports: [IonButton, IonIcon, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, HeaderComponent],
 })
 export class HomePage {
   constructor() {}
