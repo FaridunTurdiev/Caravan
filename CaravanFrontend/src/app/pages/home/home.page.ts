83a46b1d51d9ec68a3c37b7f83f1f5aeb3a9bb27
Turdiev <faridun.turdiev@softwareone.com>
2024-04-18 13:52:22 +0200

    Added Cards to the home page

--- CaravanFrontend/src/app/pages/home/home.page.ts
+++ CaravanFrontend/src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonIcon, IonButton, IonGrid, IonRow, IonCol, IonCardContent, IonCardHeader, IonCard, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { NavigationMenuComponent } from '../../shared/components/navigation-menu/navigation-menu.component';
 
@@ -9,7 +9,23 @@ import { NavigationMenuComponent } from '../../shared/components/navigation-menu
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonButton, IonIcon, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, HeaderComponent, NavigationMenuComponent],
+  imports: [IonCardSubtitle, IonCardTitle, IonCard, 
+    IonCardHeader, 
+    IonCardContent, 
+    IonCol, 
+    IonRow, 
+    IonGrid, 
+    IonButton, 
+    IonIcon, 
+    IonInput, 
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonInput, 
+    HeaderComponent, 
+    NavigationMenuComponent,
+  ],
 })
 export class HomePage {
   
